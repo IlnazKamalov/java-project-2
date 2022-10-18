be1661da86c0ea5268b901f9174641793cf379ab
IlnazKamalov <kamalovpk@gmail.com>
2022-10-18 13:42:23 +0300

    Node fix

--- app/src/main/java/hexlet/code/DiffBuilder.java
+++ app/src/main/java/hexlet/code/DiffBuilder.java
@@ -1,14 +1,12 @@
 package hexlet.code;
 
 
-
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.ArrayList;
-
-import static hexlet.code.Node.createDifference;
+import java.util.Objects;
 
 public class DiffBuilder {
     public static List<Node> buildDiff(Map<String, Object> map1, Map<String, Object> map2) {
@@ -21,5 +19,20 @@ public class DiffBuilder {
             allDifferences.add(createDifference(map1, map2, key));
         }
         return allDifferences;
+    }
+
+    protected static Node createDifference(Map<String, Object> map1, Map<String, Object> map2, String key) {
+        Object map1key = map1.get(key);
+        Object map2key = map2.get(key);
+
+        if (!map2.containsKey(key)) {
+            return new Node("removed", key, map1key, map2key);
+        } else if (!map1.containsKey(key)) {
+            return new Node("added", key, map1key, map2key);
+        } else if (Objects.equals(map1key, map2key)) {
+            return new Node("unchanged", key, map1key, map2key);
+        } else {
+            return new Node("changed", key, map1key, map2key);
+        }
     }
 }
--- app/src/main/java/hexlet/code/Node.java
+++ app/src/main/java/hexlet/code/Node.java
@@ -1,8 +1,5 @@
 package hexlet.code;
 
-import java.util.Map;
-import java.util.Objects;
-
 public class Node {
 
     private final String typeOfChange;
@@ -31,20 +28,5 @@ public class Node {
 
     public final Object getFinalValue() {
         return finalValue;
-    }
-
-    static Node createDifference(Map<String, Object> map1, Map<String, Object> map2, String key) {
-        Object map1key = map1.get(key);
-        Object map2key = map2.get(key);
-
-        if (!map2.containsKey(key)) {
-            return new Node("removed", key, map1key, map2key);
-        } else if (!map1.containsKey(key)) {
-            return new Node("added", key, map1key, map2key);
-        } else if (Objects.equals(map1key, map2key)) {
-            return new Node("unchanged", key, map1key, map2key);
-        } else {
-            return new Node("changed", key, map1key, map2key);
-        }
     }
 }
